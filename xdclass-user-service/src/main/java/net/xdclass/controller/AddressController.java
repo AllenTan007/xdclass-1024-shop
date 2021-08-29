891631d258baca0bae6b72664a39490518fb1fd7
Tan Shiwei <853212308@qq.com>
2021-08-29 09:23:40 +0800

    基础框架搭建

---
+++ xdclass-user-service/src/main/java/net/xdclass/controller/AddressController.java
@@ -0,0 +1,44 @@
+package net.xdclass.controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import net.xdclass.model.AddressDO;
+import net.xdclass.service.AddressService;
+import net.xdclass.util.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 电商-公司收发货地址表 前端控制器
+ * </p>
+ *
+ * @author 二当家小D
+ * @since 2021-08-28
+ */
+@Api("收货地址模块")
+@RestController
+@RequestMapping("/api/address/v1/")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping("find/{address_id}")
+    @ApiOperation("根据id查询地址详情")
+    public Object detail(
+            @ApiParam(value = "地址id",required = true)
+            @PathVariable("address_id") long id){
+        AddressDO addressDO = addressService.detail(id);
+        return JsonData.buildSuccess(addressDO);
+    }
+
+
+}
+
